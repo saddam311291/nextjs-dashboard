007da451c9e740f6903286a9cc0a29bb0fabd994
saddam311291 <saddam311291@gmail.com>
2024-05-28 10:40:58 +0600

    no error 11

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -46,15 +46,7 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue}  />
         <LatestInvoices latestInvoices={latestInvoices} />
-        
-        {/* <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense> */}
-       
-   
-         {/* <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense> */}
+     
       </div>
     </main>
   );
